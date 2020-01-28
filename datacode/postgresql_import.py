bd6b7a14093be34b2e19618e1c3bca0800f9ea06
0buz <ady_peiu@yahoo.com>
2020-01-28 21:29:30 +0000

    dateutil

--- datacode/postgresql_import.py
+++ datacode/postgresql_import.py
@@ -3,6 +3,7 @@ import csv
 import re
 import datacode.datasource as datasource
 from datetime import datetime
+from dateutil import parser
 import os
 #
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'evolution.settings')
@@ -20,33 +21,34 @@ cur = conn.cursor()
 # for item in res:
 #     print(item)
 
-
-def format_date(date):
-
-    if datetime.strptime(date,)
-
-
-     return date.strftime('%Y-%m-%d %H:%M:%S')
-
-    posted_date = datetime.strptime(row[-1], '%d/%m/%Y %H:%M:%S')
-    created_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+#
+# def format_date(date):
+#
+#     if datetime.strptime(date,)
+#
+#
+#      return date.strftime('%Y-%m-%d %H:%M:%S')
+#
+#     posted_date = datetime.strptime(row[-1], '%d/%m/%Y %H:%M:%S')
+#     created_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 if __name__ == "__main__":
     preprocdir = os.path.join(os.getcwd() + "/datacode/data/preprocessed")
     preproc_files = next(os.walk(preprocdir))[2]      #get only filename from walk tuple; returns list
     unprocessed_files =[file for file in preproc_files if not file.startswith("imported")]
 
-    for work_file in unprocessed_files[:2]:
+    for work_file in unprocessed_files:
         with open(str(os.path.join(preprocdir,work_file)), "r") as f:
             print(f"Processing file {work_file}")
             next(f)  # skip header
             reader = csv.reader(f)
             for row in reader:
-                posted_date = datetime.strptime(row[-1], '%d/%m/%Y %H:%M:%S')
+                posted_date = parser.parse(row[-1])   #process other date formats
+               # posted_date = datetime.strptime(row[-1], '%d/%m/%Y %H:%M:%S')
                 created_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                # print((row[:-1]+[a.strftime('%Y-%m-%d %H:%M:%S'),created_date,'2']))
                 cur.execute("INSERT INTO jobmarket_job(title, description, type, location, duration, start_date, rate, recruiter, posted_date,created_date, owner_id) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);",
-                            (row[:-1]+[posted_date.strftime('%Y-%m-%d %H:%M:%S'),created_date,'2']))
+                            (row[:-1]+[posted_date,created_date,'2']))
         print(f"Processed file {work_file}")
         old_file=os.path.join(preprocdir,work_file)
         outputname = re.sub('^preprocessed(.*)csv$', 'imported\\1csv', work_file)
